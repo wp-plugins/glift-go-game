8f462f8636e85f502e95e17a1f2f795d5d8d279d
gogameguru <gogameguru@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 23:00:08 +0000

    Deploy version 0.6.1
    git-svn-id: https://plugins.svn.wordpress.org/glift-go-game/tags/0.6.1@1090776 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/glift-eidogo.php
@@ -0,0 +1,50 @@
+<?php
+
+/** This file provides backwards compatibility with the [sgf] shortcode
+ * which was used by the old EidoGo for WordPress plugin.
+ */
+
+add_shortcode( 'sgf', 'eidogo_do_shortcode' );
+
+// sanitize, explicitly validate and arrange shortcode data
+// we will translate any attributes into Glift format, then call glift_create
+function eidogo_do_shortcode( $atts, $content, $tag ) {
+	
+	// did our shortcode send any data?
+	if ( $atts ) {
+		// if so then clean it up
+		$clean_atts = array_map( 'sanitize_text_field', $atts );
+
+		// look for sgf data
+		if ( isset( $clean_atts['sgfurl'] ) ) {
+			$glift_atts['sgf'] = $clean_atts['sgfurl'];
+			
+		// no sgfurl, so do we have $content?
+		} elseif ( $content ) {
+			$clean_content = sanitize_text_field( $content );
+			$glift_atts['sgf'] = $clean_content;
+
+		} else {
+			// we don't have any data, so return false
+			return FALSE;
+		}
+	
+		/* sgfDefaults */
+		if ( isset( $clean_atts['theme'] ) && 
+		'problem' == $clean_atts['theme'] ) 
+		$glift_atts['widgetType'] = 'STANDARD_PROBLEM';
+		
+
+	// we didn't receive any shortcode atts, so let's look for content
+	} elseif ( $content ) {
+		$clean_content = sanitize_text_field( $content );
+		$glift_atts['sgf'] = $clean_content;
+
+	} else {
+		// we don't have any data, so return false
+		return FALSE;
+	}
+
+	// we have some data, so create the Glift object and output as HTML
+	return glift_create( $glift_atts, FALSE, FALSE ); // only pass $glift_atts
+}
